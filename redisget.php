ac2347ecaf592c13b0af5f3223dff7621125e242
Surge-Ricky <surgericky@gmail.com>
2021-08-26 10:34:30 -0500

    Update redisget.php

--- redisget.php
+++ redisget.php
@@ -2,7 +2,7 @@
    //Connecting to Redis server on localhost 
    $redis = new Redis(); 
    $redis->connect('redis', 6379); 
-   echo "Connection to server sucessfully\n"; 
+   //echo "Connection to server sucessfully\n"; 
    //set the data in redis string 
    // Get the stored data and print it 
    //$example_data = $redis->get("testdata");
