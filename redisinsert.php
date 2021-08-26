9b3a4a3e53c654c5cba8d678bff0439da3f334cc
Surge-Ricky <surgericky@gmail.com>
2021-08-26 08:03:43 -0500

    Update redisinsert.php

--- redisinsert.php
+++ redisinsert.php
@@ -4,7 +4,7 @@
    $redis->connect('redis', 6379); 
    echo "Connection to server sucessfully\n"; 
    //set the data in redis string 
-   $redis->set("testdata", "this is a test"); 
+   $redis->set("testdata", "this is a testtt"); 
    // Get the stored data and print it 
    $example_data = $redis->get("testdata");
    echo "Inserted Data: $example_data";
