5f33c1b83525f45a4ddd6f031e6614841f9fd34f
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-08-26 09:37:44 -0400

    redis_config fix
    commented out the echo

--- redis_config.php
+++ redis_config.php
@@ -2,5 +2,5 @@
     //Connecting to Redis server on localhost 
     $redis = new Redis(); 
     $redis->connect('redis', 6379); 
-    echo "Connection to server sucessfully\n"; 
+    //echo "Connection to server sucessfully\n"; 
 ?>
