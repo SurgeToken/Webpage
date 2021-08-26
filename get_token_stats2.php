b0d003f00417696d94500e610f569760905b3117
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-08-26 09:34:51 -0400

    stupidity check
    deleted $data = array(); by mistake

--- get_token_stats2.php
+++ get_token_stats2.php
@@ -2,28 +2,19 @@
 
     //Connecting to Redis server on localhost 
     include("redis_config.php");
+    
+    $data = array();
 
-    $susd_holders = $redis->get("sUSD Holders");
-    $susd_total_supply = $redis->get("sUSD Total Supply");
-    $busd_total_balance = $redis->get("bUSD Total Balance");
-    $busd_price = $redis->get("bUSD Price");
-    $susd_price = $redis->get("sUSD Price");
-    $seth_holders = $redis->get("sETH Holders");
-    $total_supply_seth = $redis->get("sETH Total Supply");
-    $total_balance_weth = $redis->get("wETH Total Balance");
-    $weth_price = $redis->get("wETH Price");
-    $seth_price = $redis->get("sETH Price");
-
-    $row['susd_holders'] = $susd_holders;
-    $row['total_supply_susd'] = $susd_total_supply;
-    $row['total_balance_busd'] = $busd_total_balance;
-    $row['busd_price'] = $busd_price;
-    $row['susd_price'] = $susd_price;
-    $row['seth_holders'] = $seth_holders;
-    $row['total_supply_seth'] = $total_supply_seth;
-    $row['total_balance_weth'] = $total_balance_weth;
-    $row['weth_price'] = $weth_price;
-    $row['seth_price'] = $seth_price;
+    $row['susd_holders'] = $redis->get("sUSD Holders");
+    $row['total_supply_susd'] = $redis->get("sUSD Total Supply");
+    $row['total_balance_busd'] = $redis->get("bUSD Total Balance");
+    $row['busd_price'] = $redis->get("bUSD Price");
+    $row['susd_price'] = $redis->get("sUSD Price");
+    $row['seth_holders'] = $redis->get("sETH Holders");
+    $row['total_supply_seth'] = $redis->get("sETH Total Supply");
+    $row['total_balance_weth'] = $redis->get("wETH Total Balance");
+    $row['weth_price'] = $redis->get("wETH Price");
+    $row['seth_price'] = $redis->get("sETH Price");
 
     //push all $row variables into the $data array
     array_push($data, $row);
