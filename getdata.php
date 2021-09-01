db3c34851ea60984383b4e8d7aed1fe86a06cc5e
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-09-01 15:07:26 -0400

    test

--- getdata.php
+++ getdata.php
@@ -1,18 +1,13 @@
 <?php
 
-//  Initiate curl
-$ch = curl_init();
-// Will return the response, if false it print the response
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// Set the url
-$url = "https://api.covalenthq.com/v1/56/address/0x80733dc58b98729346904241C635a5CC78Ce6df8/balances_v2/?&key=ckey_43c97667ea9547c594b5c51cf0e";
+$url1 = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x5B1d1BBDCc432213F83b15214B93Dc24D31855Ef&address=0x80733dc58b98729346904241C635a5CC78Ce6df8&tag=latest&apikey=7BY2SX3KIF1NT1QEPY82VZB2WBTJFMN75R
+";
+$url2 = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x14fEe7d23233AC941ADd278c123989b86eA7e1fF&address=0x80733dc58b98729346904241C635a5CC78Ce6df8&tag=latest&apikey=7BY2SX3KIF1NT1QEPY82VZB2WBTJFMN75R
+";
 
-curl_setopt($ch, CURLOPT_URL,$url);
-// Execute
-$result=curl_exec($ch);
-// Closing
-curl_close($ch);
+$json = file_get_contents($url1);
+$obj = json_decode($json);
+echo $obj
 
-// Will dump a beauty json :3
-var_dump(json_decode($result, true));
+
 ?>
