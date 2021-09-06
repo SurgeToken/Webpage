bda4c45753aaecea01047c2e6ddae2b6bce7a546
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-09-06 14:43:16 -0400

    i4 update
    Testing new method for wallet address search

--- getdata.php
+++ getdata.php
@@ -4,77 +4,79 @@
     include("redis_config.php");
 
     $data = array();
+    $api_key = "7BY2SX3KIF1NT1QEPY82VZB2WBTJFMN75R";
 
     $wallet_address = $_POST["walletAddress"];
 
-    //token_wallet_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=". $token_address ."&address=". $wallet_address ."&tag=latest&apikey=7BY2SX3KIF1NT1QEPY82VZB2WBTJFMN75R";
+    $tokens_array = array("SUSD"=>"0x14fEe7d23233AC941ADd278c123989b86eA7e1fF", "SETH"=>"0x5B1d1BBDCc432213F83b15214B93Dc24D31855Ef");
 
-    $seth_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x5B1d1BBDCc432213F83b15214B93Dc24D31855Ef&address=". $wallet_address ."&tag=latest&apikey=7BY2SX3KIF1NT1QEPY82VZB2WBTJFMN75R";
-    $susd_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x14fEe7d23233AC941ADd278c123989b86eA7e1fF&address=".$wallet_address."&tag=latest&apikey=7BY2SX3KIF1NT1QEPY82VZB2WBTJFMN75R
-    ";
+    foreach($tokens_array as $token_name => $token_address) {
 
-    $json_seth = file_get_contents($seth_url);
-    $json_susd = file_get_contents($susd_url);
+        $token_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=".$token_address."&address=". $wallet_address ."&tag=latest&apikey=".$api_key."";
 
-    $seth = json_decode($json_seth);
-    $susd = json_decode($json_susd);
+        $json_token = file_get_contents($token_url);
+        $token = json_decode($json_token);
 
-    $seth_result = $seth->result;
-    $susd_result =  $susd->result;
+        $token_result = $token->result;
 
-    /* sUSD Results */
+        if($token_result != 0){
+            
+            switch($token_name){
+                case "SETH":
+                    //get current price of wETH
+                    $get_utoken_price = $redis->get("wETH Price");
+                            
+                    //calculate sETH Price
+                    $token_price = $redis->get("sETH Price");
 
-        //calculate sUSD Price
-        $susd_price = $redis->get("sUSD Price");
+                    $u_token = "ETH";
+                    $s_token = "sETH";
 
-        //get the current price of BNB
-        $bnb_price = $redis->get("BNB Price");
+                    //calculate the USD value of sETH
+                    $user_token_value = $token_price * $token_result;
+                    $user_token_value_trimmed = rtrim(sprintf('%.4f', floatval($user_token_value)),'0');
                     
-        //calculate the USD value of sUSD
-        $user_susd_usd_price = $susd_price * $susd_result;
-        $user_susd_usd_price_trimmed = rtrim(sprintf('%.2f', floatval($user_susd_usd_price)),'0');
+                    //calculate users value in ETH
+                    $user_usd_value = $user_token_value * $get_utoken_price;
+                    $user_usd_value_trimmed = rtrim(sprintf('%.2f', floatval($user_usd_value)),'0');
+                    break;
+                case "SUSD":
+                    //get the current price of BNB
+                    $get_utoken_price = $redis->get("BNB Price");
 
-        //calculate users value in BNB
-        $user_bnb_value = $user_susd_usd_price / $bnb_price;
-        $user_bnb_value_trimmed = rtrim(sprintf('%.12f', floatval($user_bnb_value)),'0');
-        
-        //send data to array
-        $row['susd_amount'] = $susd_result;
-        $row['value_bnb'] = $user_bnb_value_trimmed;
-        $row['value_susd_usd'] = number_format($user_susd_usd_price_trimmed, 2, '.', ',');
+                    //calculate sUSD Price
+                    $token_price = $redis->get("sUSD Price");
 
+                    $u_token = "BNB";
+                    $s_token = "sUSD";
 
-    /* sETH Results */
-
-        //get current price of wETH
-        $get_weth_price = $redis->get("wETH Price");
+                    //calculate the USD value of sETH
+                    $user_token_value = $token_price * $token_result;
+                    $user_token_value_trimmed = rtrim(sprintf('%.2f', floatval($user_token_value)),'0');
                     
-        //calculate sETH Price
-        $seth_price = $redis->get("sETH Price");
+                    //calculate users value in ETH
+                    $user_usd_value = $user_token_value * $get_utoken_price;
+                    $user_usd_value_trimmed = rtrim(sprintf('%.12f', floatval($user_usd_value)),'0');
+                    break;
 
-        //calculate the value of sETH
-        $user_seth_value = $seth_price * $seth_result;
-        $user_seth_value_trimmed = rtrim(sprintf('%.4f', floatval($user_seth_value)),'0');
-        
-        //calculate users value in ETH
-        $user_usd_value = $user_seth_value * $get_weth_price;
-        $user_usd_value_trimmed = rtrim(sprintf('%.2f', floatval($user_usd_value)),'0');
+            }
 
-        //send data to array
-        $row['seth_amount'] = $seth_result;
-        $row['value_eth'] = $user_seth_value_trimmed;
-        $row['value_seth_usd'] = number_format($user_usd_value_trimmed, 2, '.', ',');
+            //send data to array
+            $row['token_amount'] = $token_result;
+            $row['u_token'] = $u_token;
+            $row['s_token'] = $s_token;
+            $row['value_utoken'] = $user_token_value_trimmed;
+            $row['value_token_usd'] = number_format($user_usd_value_trimmed, 2, '.', ',');
 
+        }
 
-    /* sBTC Results */
+        //push all $row variables into the $data array
+        array_push($data, $row);
+         
+    }
 
-    //push all $row variables into the $data array
-    array_push($data, $row);
-    
     //push the $data array to index.html
     echo json_encode($data); 
-
-
 
 
 ?>
