80164923a7236ff60c8f4eab46b75988697e8741
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-09-06 16:10:37 -0400

    i4 testing 11

--- getdata.php
+++ getdata.php
@@ -51,12 +51,14 @@
                     $s_token = "sETH";
 
                     //calculate the USD value of sETH
-                    $user_token_usd_value = $seth_price * $token_result;
-                    $user_token_usd_value_trimmed = rtrim(sprintf('%.2f', floatval($user_token_usd_value)),'0');
+                    $user_token_usd_value = $user_token_value / $get_weth_price;
+                    $user_token_usd_value_trimmed = rtrim(sprintf('%.4f', floatval($user_token_usd_value)),'0');
+                   
                     
                     //calculate users value in ETH
-                    $user_token_value = $user_token_usd_value / $get_weth_price;
-                    $user_token_value_trimmed = rtrim(sprintf('%.4f', floatval($user_token_value)),'0');
+                    $user_token_value = $seth_price * $token_result;
+                    $user_token_value_trimmed = rtrim(sprintf('%.2f', floatval($user_token_value)),'0');
+                    
                     break;
                 default:
                     break;
