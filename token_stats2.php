0f18ccc7649d71de42b81200d9cb053c2214b5a1
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-09-16 19:19:18 -0400

    testing covalent

--- token_stats2.php
+++ token_stats2.php
@@ -4,26 +4,25 @@
     //Connecting to Redis server on localhost 
     include("redis_config.php");
    
-    $api_key = "7BY2SX3KIF1NT1QEPY82VZB2WBTJFMN75R";
+    $b_api_key = "7BY2SX3KIF1NT1QEPY82VZB2WBTJFMN75R";
+    $c_api_key = "ckey_43c97667ea9547c594b5c51cf0e";
 
 
     /* SurgeUSD Stats */
 
         //get total supply for sUSD
-        $susd_token_total_supply_url = "https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=0x14fee7d23233ac941add278c123989b86ea7e1ff&apikey=".$api_key."";
+        $susd_token_total_supply_url = "https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=0x14fee7d23233ac941add278c123989b86ea7e1ff&apikey=".$b_api_key."";
 
         $susd_total_supply_json = file_get_contents($susd_token_total_supply_url);
         $susd_token_total_supply = json_decode($susd_total_supply_json);
         $susd_total_supply = $susd_token_total_supply->result;
-        echo $susd_total_supply + "<br/>";
 
         //get total balance of busd
-        $busd_token_total_balance_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xe9e7cea3dedca5984780bafc599bd69add087d56&address=0x14fee7d23233ac941add278c123989b86ea7e1ff&tag=latest&apikey=".$api_key."";
+        $busd_token_total_balance_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xe9e7cea3dedca5984780bafc599bd69add087d56&address=0x14fee7d23233ac941add278c123989b86ea7e1ff&tag=latest&apikey=".$b_api_key."";
 
         $busd_total_balance_json = file_get_contents($busd_token_total_balance_url);
         $busd_token_total_balance = json_decode($busd_total_balance_json);
         $busd_total_balance = $busd_token_total_balance->result;
-        echo $busd_total_balance + "<br/>";
 
         //get data from BSCScan for sUSD & bUSD
         $get_html_susd = file_get_html('https://bscscan.com/token/0x14fee7d23233ac941add278c123989b86ea7e1ff');
@@ -31,14 +30,19 @@
         
         //store data into variables
         $susd_holders = $get_html_susd->find('div[class="mr-3"]',0)->plaintext;
-        $busd_price = $get_html_busd->find('div[id="ContentPlaceHolder1_tr_valuepertoken"]',0)->plaintext;
+
+        //get busd price from covalent
+        $busd_price_url = "https://api.covalenthq.com/v1/pricing/historical_by_addresses_v2/56/USD/0xe9e7cea3dedca5984780bafc599bd69add087d56/?&key="+$c_api_key+"";
+        $busd_price_json = file_get_contents($busd_price_url);
+        $busd_price_decoded = json_decode($busd_price_json);
+        $busd_price = $busd_price_decoded->data->prices->price;
+        echo $busd_price;
         
         //format busd price
         $busd_price_trimmed = substr($busd_price, 12, 6);  
 
         //calculate sUSD Price
         $susd_price = $busd_total_balance / $susd_total_supply;
-        echo $susd_price;
 
         //format susd price 
         $susd_trimmed = rtrim(sprintf('%.16f', floatval($susd_price)),'0');
@@ -50,11 +54,23 @@
         $bnb_price_json = file_get_contents($bnb_price_url);
         $bnb_price_encoded = json_decode($bnb_price_json);
         $bnb_price = $bnb_price_encoded->result->ethusd;
-        echo $bnb_price;
         
     
     /* SurgeETH Stats */
 
+        //get total supply for sETH
+        $seth_token_total_supply_url = "https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=0x5b1d1bbdcc432213f83b15214b93dc24d31855ef&apikey=".$api_key."";
+
+        $seth_total_supply_json = file_get_contents($seth_token_total_supply_url);
+        $seth_token_total_supply = json_decode($seth_total_supply_json);
+        $seth_total_supply = $seth_token_total_supply->result;
+
+        //get total balance of bETH
+        $beth_token_total_balance_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x2170ed0880ac9a755fd29b2688956bd959f933f8&address=0x5b1d1bbdcc432213f83b15214b93dc24d31855ef&tag=latest&apikey=".$api_key."";
+
+        $beth_total_balance_json = file_get_contents($beth_token_total_balance_url);
+        $beth_token_total_balance = json_decode($beth_total_balance_json);
+        $beth_total_balance = $beth_token_total_balance->result;
 
         //get data from BSCScan for sETH & wETH
         $get_html_seth = file_get_html('https://bscscan.com/token/0x5b1d1bbdcc432213f83b15214b93dc24d31855ef');
@@ -62,8 +78,6 @@
 
         //store data into variables
         $seth_holders = $get_html_seth->find('div[class="mr-3"]',0)->plaintext;
-        $total_supply_seth = $get_html_seth->find('span[class="hash-tag text-truncate"]',0)->plaintext;
-        $total_balance_weth = $get_html_weth->find('div[id="ContentPlaceHolder1_divFilteredHolderBalance"]',0)->plaintext;
         $weth_price = $get_html_weth->find('div[id="ContentPlaceHolder1_tr_valuepertoken"]',0)->plaintext;
 
         //strip commas from sETH
@@ -165,7 +179,7 @@
         $redis->set("bUSD Total Balance", trim($total_balance_busd_no_commas));
         $redis->set("bUSD Price", trim($busd_price_trimmed));
         $redis->set("sUSD Price", trim($susd_trimmed));
-        $redis->set("BNB Price", trim($bnb_price_trimmed));
+        $redis->set("BNB Price", $bnb_price);
 
         $redis->set("sETH Holders", trim($seth_holders));
         $redis->set("sETH Total Supply", trim($total_supply_seth_no_commas));
@@ -186,7 +200,7 @@
         $redis->set("sADA Price", trim($sada_trimmed)); */
 
         /* $redis->set("BNB Price-Test", $bnb_price);
-        $bnb_price_test = $redis->get("BNB Price-Test"); */
+        $bnb_price_test = $redis->get("BNB Price-Test");
         
         echo "1";
     
