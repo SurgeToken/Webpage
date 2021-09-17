63c008ac165b85bca287efd96c24e6f8a6e742b5
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-09-17 09:30:21 -0400

    TS Test 3

--- token_stats2.php
+++ token_stats2.php
@@ -32,8 +32,7 @@
         $busd_price_url = "https://api.covalenthq.com/v1/pricing/historical_by_addresses_v2/56/USD/0xe9e7cea3dedca5984780bafc599bd69add087d56/?&key=ckey_43c97667ea9547c594b5c51cf0e";
         $busd_price_json = json_decode(file_get_contents($busd_price_url), true);
 
-        $busd_price = $busd_price_json['data']['prices'][0]['price'];
-        echo $busd_price;
+        print_r($busd_price_json);
 
         
 
