d7a3bf3ddf883f2cdfb0045bf9f89ccb1dacec90
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-09-17 11:28:09 -0400

    TS Test BSC 11

--- token_stats2.php
+++ token_stats2.php
@@ -14,14 +14,15 @@
 
         $susd_total_supply_json = json_decode(file_get_contents($susd_token_total_supply_url));
         $susd_total_supply = $susd_total_supply_json->result;
-        print_r($susd_total_supply + " ");
+        print_r($susd_total_supply);
+        echo '<br/>';
 
         //get total balance of busd
         $busd_token_total_balance_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xe9e7cea3dedca5984780bafc599bd69add087d56&address=0x14fee7d23233ac941add278c123989b86ea7e1ff&tag=latest&apikey=".$b_api_key."";
 
         $busd_total_balance_json = json_decode(file_get_contents($busd_token_total_balance_url));
         $busd_total_balance = $busd_total_balance_json->result;
-        print_r($busd_total_balance + " ");
+        print_r($busd_total_balance);
 
         //get data from BSCScan for sUSD & bUSD
         $get_html_susd = file_get_html('https://bscscan.com/token/0x14fee7d23233ac941add278c123989b86ea7e1ff');
@@ -55,14 +56,15 @@
 
         $seth_total_supply_json = json_decode(file_get_contents($seth_token_total_supply_url));
         $seth_total_supply = $seth_total_supply_json->result;
-        print_r($seth_total_supply + " ");
+        print_r($seth_total_supply);
+        echo '<br/>';
 
         //get total balance of bETH
         $beth_token_total_balance_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x2170ed0880ac9a755fd29b2688956bd959f933f8&address=0x5b1d1bbdcc432213f83b15214b93dc24d31855ef&tag=latest&apikey=".$b_api_key."";
 
         $beth_total_balance_json = json_decode(file_get_contents($beth_token_total_balance_url));
         $beth_total_balance = $beth_total_balance_json->result;
-        print_r($beth_total_balance + " ");
+        print_r($beth_total_balance);
 
         //get data from BSCScan for sETH & wETH
         $get_html_seth = file_get_html('https://bscscan.com/token/0x5b1d1bbdcc432213f83b15214b93dc24d31855ef');
@@ -90,14 +92,15 @@
 
         $sbtc_total_supply_json = json_decode(file_get_contents($sbtc_token_total_supply_url));
         $sbtc_total_supply = $sbtc_total_supply_json->result;
-        print_r($sbtc_total_supply + " ");
+        print_r($sbtc_total_supply);
+        echo '<br/>';
 
         //get total balance of bBTC
         $bbtc_token_total_balance_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c&address=0xb68c9D9BD82BdF4EeEcB22CAa7F3Ab94393108a1&tag=latest&apikey=".$b_api_key."";
 
         $bbtc_total_balance_json = json_decode(file_get_contents($bbtc_token_total_balance_url));
         $bbtc_total_balance = $bbtc_total_balance_json->result;
-        print_r($bbtc_total_balance + " ");
+        print_r($bbtc_total_balance);
 
         //get data from BSCScan for sBTC Holders
         $get_html_sbtc = file_get_html('https://bscscan.com/token/0xb68c9D9BD82BdF4EeEcB22CAa7F3Ab94393108a1');
