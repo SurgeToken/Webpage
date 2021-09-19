49dd59642e406f42c30aa5301c0edcbc4bf13196
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-09-19 14:21:36 -0400

    idk

--- getdata.php
+++ getdata.php
@@ -78,7 +78,6 @@
                     
                 //calculate users value in BTC
                 $user_token_usd_value = $user_token_value * $get_btcb_price;
-                echo $get_btcb_price;
                 $user_token_usd_value_trimmed = rtrim(sprintf('%.2f', floatval($user_token_usd_value)),'0');
                 break;
             case "SADA":
