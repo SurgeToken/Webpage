cd80367f49c11013d2f81de471829336785f2539
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-09-19 16:02:42 -0400

    token_stats Sleep Update

--- token_stats2.php
+++ token_stats2.php
@@ -14,15 +14,12 @@
 
         $susd_total_supply_json = json_decode(file_get_contents($susd_token_total_supply_url));
         $susd_total_supply = $susd_total_supply_json->result;
-        print_r($susd_total_supply);
-        echo '<br/>';
 
         //get total balance of busd
         $busd_token_total_balance_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xe9e7cea3dedca5984780bafc599bd69add087d56&address=0x14fee7d23233ac941add278c123989b86ea7e1ff&tag=latest&apikey=".$b_api_key."";
 
         $busd_total_balance_json = json_decode(file_get_contents($busd_token_total_balance_url));
         $busd_total_balance = $busd_total_balance_json->result;
-        print_r($busd_total_balance);
 
         //get data from BSCScan for sUSD & bUSD
         $get_html_susd = file_get_html('https://bscscan.com/token/0x14fee7d23233ac941add278c123989b86ea7e1ff');
@@ -56,15 +53,12 @@
 
         $seth_total_supply_json = json_decode(file_get_contents($seth_token_total_supply_url));
         $seth_total_supply = $seth_total_supply_json->result;
-        print_r($seth_total_supply);
-        echo '<br/>';
 
         //get total balance of bETH
         $beth_token_total_balance_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x2170ed0880ac9a755fd29b2688956bd959f933f8&address=0x5b1d1bbdcc432213f83b15214b93dc24d31855ef&tag=latest&apikey=".$b_api_key."";
 
         $beth_total_balance_json = json_decode(file_get_contents($beth_token_total_balance_url));
         $beth_total_balance = $beth_total_balance_json->result;
-        print_r($beth_total_balance);
 
         //get data from BSCScan for sETH & wETH
         $get_html_seth = file_get_html('https://bscscan.com/token/0x5b1d1bbdcc432213f83b15214b93dc24d31855ef');
@@ -81,7 +75,6 @@
         //calculate sETH Price
         $seth_price = $beth_total_balance / $seth_total_supply;
         
-
         //format sETH price
         $seth_trimmed = rtrim(sprintf('%.16f', floatval($seth_price)),'0');
     }
@@ -92,15 +85,12 @@
 
         $sbtc_total_supply_json = json_decode(file_get_contents($sbtc_token_total_supply_url));
         $sbtc_total_supply = $sbtc_total_supply_json->result;
-        print_r($sbtc_total_supply);
-        echo '<br/>';
 
         //get total balance of bBTC
         $bbtc_token_total_balance_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c&address=0xb68c9D9BD82BdF4EeEcB22CAa7F3Ab94393108a1&tag=latest&apikey=".$b_api_key."";
 
         $bbtc_total_balance_json = json_decode(file_get_contents($bbtc_token_total_balance_url));
         $bbtc_total_balance = $bbtc_total_balance_json->result;
-        print_r($bbtc_total_balance);
 
         //get data from BSCScan for sBTC Holders
         $get_html_sbtc = file_get_html('https://bscscan.com/token/0xb68c9D9BD82BdF4EeEcB22CAa7F3Ab94393108a1');
@@ -121,83 +111,78 @@
     }
 
     function sADA(){
-        /* //get data from BSCScan for sADA & bADA
-        $get_html_sada = file_get_html('https://bscscan.com/token/0x5b1d1bbdcc432213f83b15214b93dc24d31855ef');
-        $get_html_bada = file_get_html('https://bscscan.com/token/0x3ee2200efb3400fabb9aacf31297cbdd1d435d47?a=0x5b1d1bbdcc432213f83b15214b93dc24d31855ef');
+        //get total supply for sADA
+        $sada_token_total_supply_url = "https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=0xbF6bB9b8004942DFb3C1cDE3Cb950AF78ab8A5AF&apikey=".$b_api_key."";
 
-        //store data into variables
+        $sada_total_supply_json = json_decode(file_get_contents($sada_token_total_supply_url));
+        $sada_total_supply = $sada_total_supply_json->result;
+
+        //get total balance of bADA
+        $bada_token_total_balance_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x3ee2200efb3400fabb9aacf31297cbdd1d435d47&address=0xbF6bB9b8004942DFb3C1cDE3Cb950AF78ab8A5AF&tag=latest&apikey=".$b_api_key."";
+
+        $bada_total_balance_json = json_decode(file_get_contents($bada_token_total_balance_url));
+        $bada_total_balance = $bada_total_balance_json->result;
+
+        //get data from BSCScan for sADA & bADA
+        $get_html_sada = file_get_html('https://bscscan.com/token/0xbF6bB9b8004942DFb3C1cDE3Cb950AF78ab8A5AF');
         $sada_holders = $get_html_sada->find('div[class="mr-3"]',0)->plaintext;
-        $total_supply_sada = $get_html_sada->find('span[class="hash-tag text-truncate"]',0)->plaintext;
-        $total_balance_bada = $get_html_bada->find('div[id="ContentPlaceHolder1_divFilteredHolderBalance"]',0)->plaintext;
-        $bada_price = $get_html_bada->find('div[id="ContentPlaceHolder1_tr_valuepertoken"]',0)->plaintext;
+            
+        //get bADA price from covalent
+        $bada_price_url = "https://api.covalenthq.com/v1/pricing/historical_by_addresses_v2/56/USD/0x3ee2200efb3400fabb9aacf31297cbdd1d435d47/?&key=ckey_43c97667ea9547c594b5c51cf0e";
 
-        //strip commas from sADA
-        $total_supply_sada_no_commas = str_replace(',', '', $total_supply_sada);
+        $bada_price_json = json_decode(file_get_contents($bada_price_url), true);
 
-        //strip commas from bADA
-        $bada_price_trimmed = substr($bada_price, 12, 8);
-        $bada_price_no_commas = str_replace(',', '', $bada_price_trimmed);
+        $bada_price = $bada_price_json['data'][0]['prices'][0]['price'];
 
-        //remove unneeded data from total balance bADA
-        $total_balance_bada_trimmed = substr($total_balance_bada, 8, -5);
-
-        //strip commas from total balance bADA
-        $total_balance_bada_no_commas = str_replace(',', '', $total_balance_bada_trimmed);
-
-        //remove unneeded data from current price of bADA
-        $bada_price_trimmed = substr($bada_price, 12, 6);
-        
         //calculate sADA Price
-        $sada_price = $total_balance_bada_no_commas / $total_supply_sada_no_commas;
+        $sada_price = $bada_total_balance / $sada_total_supply;
 
         //format sADA price
-        $sada_trimmed = rtrim(sprintf('%.16f', floatval($sada_price)),'0'); */
+        $sada_trimmed = rtrim(sprintf('%.16f', floatval($sada_price)),'0');
     }
 
     sUSD();
+    sleep(2);
     sETH();
+    sleep(2);
     sBTC();
-    /* sADA();
-    sleep(2); */
-
-        
+    sleep(2);
+    sADA();
+    sleep(2);
 
     
     //set the data in redis string 
 
         //sUSD-bUSD
-        /* $redis->set("sUSD Holders", trim($susd_holders));
+        $redis->set("sUSD Holders", trim($susd_holders));
         $redis->set("sUSD Total Supply", trim($susd_total_supply));
         $redis->set("bUSD Total Balance", trim($busd_total_balance));
         $redis->set("bUSD Price", trim($busd_price));
         $redis->set("sUSD Price", trim($susd_trimmed));
-        $redis->set("BNB Price", trim($bnb_price)); */
+        $redis->set("BNB Price", trim($bnb_price));
         
 
         //sETH-bETH
-        /* $redis->set("sETH Holders", trim($seth_holders));
+        $redis->set("sETH Holders", trim($seth_holders));
         $redis->set("sETH Total Supply", trim($seth_total_supply));
         $redis->set("bETH Total Balance", trim($beth_total_balance));
         $redis->set("bETH Price", trim($beth_price));
-        $redis->set("sETH Price", trim($seth_trimmed)); */
+        $redis->set("sETH Price", trim($seth_trimmed));
 
         //sBTC-bBTC
-        /* $redis->set("sBTC Holders", trim($sbtc_holders));
+        $redis->set("sBTC Holders", trim($sbtc_holders));
         $redis->set("sBTC Total Supply", trim($sbtc_total_supply));
         $redis->set("bBTC Total Balance", trim($bbtc_total_balance));
-        $redis->set("bBTC Price", trim($sbtc_price));
-        $redis->set("sBTC Price", trim($sbtc_trimmed)); */
+        $redis->set("bBTC Price", trim($bbtc_price));
+        $redis->set("sBTC Price", trim($sbtc_trimmed));
         
         
-        /* $redis->set("sADA Holders", trim($sada_holders));
+        $redis->set("sADA Holders", trim($sada_holders));
         $redis->set("sADA Total Supply", trim($total_supply_sada_no_commas));
         $redis->set("bADA Total Balance", trim($total_balance_bada_no_commas));
         $redis->set("bADA Price", trim($bada_price_no_commas));
-        $redis->set("sADA Price", trim($sada_trimmed)); */
+        $redis->set("sADA Price", trim($sada_trimmed));
 
-        /* $redis->set("BNB Price-Test", $bnb_price);
-        $bnb_price_test = $redis->get("BNB Price-Test");*/
-        
-        //echo "1";
+        echo "1";
     
 ?>
