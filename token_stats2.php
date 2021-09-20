9c7c99f99d9a2866039ef823080666db78dcffb5
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-09-20 10:14:56 -0400

    TS Test 6(sUSD)

--- token_stats2.php
+++ token_stats2.php
@@ -1,22 +1,23 @@
 <?php
-    include("simple_html_dom.php");
-
-    //Connecting to Redis server on localhost 
-    include("redis_config.php");
+    
    
-    $b_api_key = "7BY2SX3KIF1NT1QEPY82VZB2WBTJFMN75R";
-
 
     //token functions
     function sUSD(){
+
+        include("simple_html_dom.php");
+
+        //Connecting to Redis server on localhost 
+        include("redis_config.php");
+
         //get total supply for sUSD
-        $susd_token_total_supply_url = "https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=0x14fee7d23233ac941add278c123989b86ea7e1ff&apikey=".$b_api_key."";
+        $susd_token_total_supply_url = "https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=0x14fee7d23233ac941add278c123989b86ea7e1ff&apikey=7BY2SX3KIF1NT1QEPY82VZB2WBTJFMN75R";
 
         $susd_total_supply_json = json_decode(file_get_contents($susd_token_total_supply_url));
         $susd_total_supply = $susd_total_supply_json->result;
 
         //get total balance of busd
-        $busd_token_total_balance_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xe9e7cea3dedca5984780bafc599bd69add087d56&address=0x14fee7d23233ac941add278c123989b86ea7e1ff&tag=latest&apikey=".$b_api_key."";
+        $busd_token_total_balance_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xe9e7cea3dedca5984780bafc599bd69add087d56&address=0x14fee7d23233ac941add278c123989b86ea7e1ff&tag=latest&apikey=7BY2SX3KIF1NT1QEPY82VZB2WBTJFMN75R";
 
         $busd_total_balance_json = json_decode(file_get_contents($busd_token_total_balance_url));
         $busd_total_balance = $busd_total_balance_json->result;
@@ -40,26 +41,18 @@
         $susd_trimmed = rtrim(sprintf('%.16f', floatval($susd_price)),'0');
 
         //get the current price of BNB
-        $bnb_price_url = "https://api.bscscan.com/api?module=stats&action=bnbprice&apikey=".$b_api_key."";
+        $bnb_price_url = "https://api.bscscan.com/api?module=stats&action=bnbprice&apikey=7BY2SX3KIF1NT1QEPY82VZB2WBTJFMN75R";
 
         $bnb_price_json = file_get_contents($bnb_price_url);
         $bnb_price_encoded = json_decode($bnb_price_json);
         $bnb_price = $bnb_price_encoded->result->ethusd;
 
-        $redis->set("sUSD Holders", trim($susd_holders));
-        $redis->set("bUSD Price", trim($busd_price));
-        $redis->set("sUSD Price", trim($susd_trimmed));
-        $redis->set("BNB Price", trim($bnb_price));
+        $redis->set("susd_holders", trim($susd_holders));
+        /* $redis->set("bUSD Price", trim($busd_price)); */
+        /* $redis->set("sUSD Price", trim($susd_trimmed)); */
+        /* $redis->set("BNB Price", trim($bnb_price)); */
 
-        $redis_susd_holders = $redis->get("sUSD Holders");
-        $redis_busd_price = $redis->get("bUSD Price");
-        $redis_susd_price = $redis->get("sUSD Price");
-        $redis_bnb_price = $redis->get("BNB Price");
-
-        print_r( $redis_susd_holders );
-        print_r( $redis_busd_price );
-        print_r( $redis_susd_price );
-        print_r( $redis_bnb_price );
+        print_r( $redis->get("susd_holders") );
 
     }
 
