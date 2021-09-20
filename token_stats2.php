423200d212a6c52c9a503d968fbdfa1004de9ca2
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-09-20 12:12:35 -0400

    TS Test 2(sADA)

--- token_stats2.php
+++ token_stats2.php
@@ -163,13 +163,13 @@
         include("redis_config.php");
 
         //get total supply for sADA
-        $sada_token_total_supply_url = "https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=0xbF6bB9b8004942DFb3C1cDE3Cb950AF78ab8A5AF&apikey=".$b_api_key."";
+        $sada_token_total_supply_url = "https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=0xbF6bB9b8004942DFb3C1cDE3Cb950AF78ab8A5AF&apikey=7BY2SX3KIF1NT1QEPY82VZB2WBTJFMN75R";
 
         $sada_total_supply_json = json_decode(file_get_contents($sada_token_total_supply_url));
         $sada_total_supply = $sada_total_supply_json->result;
 
         //get total balance of bADA
-        $bada_token_total_balance_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x3ee2200efb3400fabb9aacf31297cbdd1d435d47&address=0xbF6bB9b8004942DFb3C1cDE3Cb950AF78ab8A5AF&tag=latest&apikey=".$b_api_key."";
+        $bada_token_total_balance_url = "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x3ee2200efb3400fabb9aacf31297cbdd1d435d47&address=0xbF6bB9b8004942DFb3C1cDE3Cb950AF78ab8A5AF&tag=latest&apikey=7BY2SX3KIF1NT1QEPY82VZB2WBTJFMN75R";
 
         $bada_total_balance_json = json_decode(file_get_contents($bada_token_total_balance_url));
         $bada_total_balance = $bada_total_balance_json->result;
