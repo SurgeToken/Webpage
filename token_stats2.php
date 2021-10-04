2be22dbfdcd18ea7f41375cf4e81b99d09602da4
johnnysheps <30838677+johnnysheps@users.noreply.github.com>
2021-10-04 14:38:10 -0400

    TS Test

--- token_stats2.php
+++ token_stats2.php
@@ -233,7 +233,7 @@
 
         $useless_price_json = json_decode(file_get_contents($useless_price_url), true);
 
-        $useless_price = $useless_price_json['0x2cd2664ce5639e46c6a3125257361e01d0213657'][0]['usd'];
+        $useless_price = $useless_price_json['0x2cd2664ce5639e46c6a3125257361e01d0213657']['usd'];
         
         //calculate suseless Price
         $suseless_price = ($useless_total_balance / $suseless_total_supply) / $divisor;
